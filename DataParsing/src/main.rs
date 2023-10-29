440e5edffef835359e932c4b0ce6d6632e14579d
VsevolodZaostrovsky <zaos2013@mail.ru>
2023-10-29 16:38:56 +0300

    I teached arima in R using this dataset

--- DataParsing/src/main.rs
+++ DataParsing/src/main.rs
@@ -1,42 +1,42 @@
 mod orderbook;
 
-use crate::orderbook::{OrderBook};
+use crate::orderbook::OrderBook;
 use tqdm::tqdm;
 
+use std::{path::Path, fs::File, io::Error, io::BufReader, io::BufRead};
+
+
+fn read_file(filename: String) -> Vec<String> {
+    let mut all_names : Vec<String> = Vec::new();
+    let mut _create_path: String = "data/".to_owned();
+    _create_path.push_str(&filename);
+    
+    let path = Path::new(_create_path.as_str());
+    let display = path.display();
+
+    let file = match File::open(&path) {
+        Err(why) => panic!("Couldn't open {}: {}", display, Error::to_string(&why)),
+        Ok(file) => file,
+    };
+
+    let reader = BufReader::new(file);
+    let lines = reader.lines();
+
+    for result in lines
+    {
+        if let Ok(line) = result
+        {
+            all_names.push(format!("data/{}", line));
+        }
+    }
+    
+    all_names
+}
 
 fn main()
 {
-    let filenames_rub: Vec<&str> = vec!["data/USD_RUB_T+1__2022-10-03",
-                                         "data/USD_RUB_T+1__2022-10-04",
-                                         "data/USD_RUB_T+1__2022-10-05",
-                                         "data/USD_RUB_T+1__2022-10-06",
-                                         "data/USD_RUB_T+1__2022-10-07",
-                                        "data/USD_RUB_T+1__2022-10-10",
-                                        "data/USD_RUB_T+1__2022-10-11",
-                                        "data/USD_RUB_T+1__2022-10-12",
-                                        "data/USD_RUB_T+1__2022-10-13",
-                                        "data/USD_RUB_T+1__2022-10-14",
-                                        "data/USD_RUB_T+1__2022-10-17",
-                                        "data/USD_RUB_T+1__2022-10-18",
-                                        "data/USD_RUB_T+1__2022-10-19",
-                                        "data/USD_RUB_T+1__2022-10-20",
-                                        "data/USD_RUB_T+1__2022-10-21",
-                                        "data/USD_RUB_T+1__2022-10-24",
-                                        "data/USD_RUB_T+1__2022-10-25",
-                                        "data/USD_RUB_T+1__2022-10-26",
-                                        "data/USD_RUB_T+1__2022-10-27",
-                                        "data/USD_RUB_T+1__2022-10-28",
-                                        "data/USD_RUB_T+1__2022-10-31",
-                                        "data/USD_RUB_T+1__2022-11-01",
-                                        "data/USD_RUB_T+1__2022-11-02",
-                                        "data/USD_RUB_T+1__2022-11-03",
-                                        ];
 
-
-                                        
     OrderBook::calculate_features("data/USD_RUB_T+1__2022-10-03", "USD/RUB_T+1".to_string(), 0.0025);
-    // for &filename in tqdm(filenames_rub.iter())
-    // {
-    //     OrderBook::calculate_features(filename, "USD/RUB_T+1".to_string(), 0.0025);
-    // }
+
+
 }
